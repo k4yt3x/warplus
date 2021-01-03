6c0e75c499cb7f4803fe63ccaf443b01fa84c4fd
K4YT3X <k4yt3x@k4yt3x.com>
2021-01-03 04:53:50 -0500

    added version 1.0.0 and documentations

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# warplus
-WARP+ quota aquirement tool
+# WARPlus
+
+WARPlus helps you to get free quota on CloudFlare's WARP+.
+
+This project is based on [ALIILAPRO/warp-plus-cloudflare](https://github.com/ALIILAPRO/warp-plus-cloudflare)'s concepts. WARPlus differs by being a CLI application and supports a lot of extra features.
+
+Please do not abuse this script and use CloudFlare's services fairly. Don't let this become a tragedy of the commons  and ruin it for everyone. This script is for educational purposes only.
+
+![screenshot](https://user-images.githubusercontent.com/21986859/103475930-77397c80-4da9-11eb-8e84-a780c3e0ea21.png)
+
+## Quick Start
+
+The most basic way to use this application is shown below. `DEVICE_ID` here should be replaced with your WARP application's device ID (e.g., `9a4190b3-ab1c-465c-aa03-189aa5141ec1`). This ID can be found in `Menu > Advanced > Diagnostics > Client Configuration > ID` on mobile phones. If you're using `wgcf`, it will be in the `wgcf-account.toml` file.
+
+This command will run WARPlus with one thread using your own IP. It will send a request every 15 seconds to avoid being rate-limited by CloudFlare.
+
+```shell
+python3 warplus.py -w DEVICE_ID
+```
+
+If you'd like to do it faster, you can try multi-threading + proxies. WARPlus can retrieve a list of proxies automatically from ProxyScrape, just like [ALIILAPRO/warp-plus-cloudflare](https://github.com/ALIILAPRO/warp-plus-cloudflare)'s GUI version.
+
+The example below will launch 100 threads, use proxies, have a sending-interval of 1 second, automatically delete unusable proxies from the proxy list, and wait for the server to respond for a maximum of 10 seconds. Threads without jobs to do will exit automatically.
+
+```shell
+py3 warplus.py -w DEVICE_ID -t 100 -p -i 1 -a -o 10
+```
+
+You can also set a limit on the maximum number of times the script will send successful requests. For example, if you want to get *about* 10 gigabytes of quota, you can set `-l/--limit` to 10. Be aware that if you have too many threads, the limit might get exceeded.
+
+## Full Usages
+
+```console
+usage: warplus [-h] -w WARPID [-t THREADS] [-i INTERVAL] [-l LIMIT] [-o TIMEOUT] [-p] [-a] [-u URL] [-v]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -w WARPID, --warpid WARPID
+                        WARP device ID (default: None)
+  -t THREADS, --threads THREADS
+                        number of threads to use (default: 5)
+  -i INTERVAL, --interval INTERVAL
+                        time interval between sending two requests in one thread (default: 15)
+  -l LIMIT, --limit LIMIT
+                        set the maximum number of successful requests the script will send (default: None)
+  -o TIMEOUT, --timeout TIMEOUT
+                        server connection timeout (default: None)
+  -p, --proxies         use proxies (default: False)
+  -a, --autoremove      automatically remove unusable proxies (default: False)
+  -u URL, --url URL     URL to send the POST request to (default: https://api.cloudflareclient.com/v0a398/reg)
+  -v, --version         print WARPlus's version and exit (default: False)
+```
---
+++ warplus.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Name: WARPlus
+Author: K4YT3X
+Date Created: January 1, 2021
+Last Modified: January 3, 2021
+
+Descriptin: WARPlus is a tool similar to ALIILAPRO/warp-plus-cloudflare.
+"""
+
+# built-in imports
+import argparse
+import collections
+import datetime
+import random
+import string
+import sys
+import threading
+import time
+
+# third-party imports
+import requests
+
+
+VERSION = "1.0.0"
+
+HEADERS = {
+    "Content-Type": "application/json; charset=UTF-8",
+    "Host": "api.cloudflareclient.com",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/3.12.1",
+}
+
+
+class RequestSender(threading.Thread):
+    """RequesterSender instances are workers that send
+    POST requests to CloudFlare's servers. It inherits
+    threading.Thread and can be started/stopped like a
+    thread.
+    """
+
+    def __init__(
+        self,
+        warpid: str,
+        interval: int,
+        post_url: str,
+        limit: int,
+        timeout: int,
+        autoremove: bool,
+    ):
+        threading.Thread.__init__(self)
+        self.warpid = warpid
+        self.interval = interval
+        self.post_url = post_url
+        self.limit = limit
+        self.timeout = timeout
+        self.autoremove = autoremove
+        self.running = False
+
+    def run(self):
+        global successes
+        self.running = True
+        while self.running and (self.limit is None or successes < self.limit):
+            self.send_request()
+            time.sleep(self.interval)
+        _print(f"Thread {self.name} exiting")
+
+    def stop(self):
+        self.running = False
+        self.join()
+
+    def send_request(self):
+        """send POST request to CloudFlare's server"""
+        global successes
+        global fails
+        global proxies
+
+        succeeded = False
+        error_message = ""
+
+        try:
+
+            # generate random installation ID
+            install_id = "".join(
+                random.choices(string.ascii_uppercase + string.digits, k=22)
+            )
+
+            # construct POST request payload
+            post_data = {
+                "key": f"{''.join(random.choices(string.ascii_uppercase + string.digits, k=43))}=",
+                "install_id": install_id,
+                "fcm_token": f"{install_id}:APA91b{''.join(random.choices(string.ascii_uppercase + string.digits, k=134))}",
+                "referrer": self.warpid,
+                "warp_enabled": False,
+                "tos": datetime.datetime.utcnow().isoformat()[:-3] + "+00:00",
+                "type": "Android",
+                "locale": "en_US",
+            }
+
+            # send request
+            if proxies is None:
+                response = requests.post(
+                    self.post_url, json=post_data, headers=HEADERS, timeout=self.timeout
+                )
+
+            else:
+
+                proxy = proxies.popleft()
+
+                response = requests.post(
+                    self.post_url,
+                    json=post_data,
+                    headers=HEADERS,
+                    timeout=self.timeout,
+                    proxies={
+                        "http": f"https://{proxy}",
+                        "https": f"https://{proxy}",
+                    },
+                )
+
+            if response.status_code == requests.codes.ok:
+                successes += 1
+                succeeded = True
+            else:
+                fails += 1
+                error_message = f" with code {response.status_code}"
+
+            if proxies:
+                proxies.append(proxy)
+
+        except IndexError:
+            self.running = False
+            return
+
+        # print error and carryon on upon exceptions
+        except Exception as e:
+            # _print(f"Thread {self.name} encountered an exception")
+            # _print(traceback.format_exc(), file=sys.stderr)
+
+            fails += 1
+            error_message = f" with Python exception {e}"
+
+            if proxies and not self.autoremove:
+                proxies.append(proxy)
+
+        finally:
+            if succeeded:
+                _print(
+                    f"[Successes: {successes} | Fails: {fails} | Proxies: {len(proxies)}] Thread {self.name} succeeded",
+                    file=sys.stderr,
+                )
+
+            else:
+                _print(
+                    f"[Successes: {successes} | Fails: {fails} | Proxies: {len(proxies)}] Thread {self.name} failed{error_message}",
+                    file=sys.stderr,
+                )
+
+
+def _print(message: str, **kwargs):
+    """thread-safe print
+
+    Args:
+        message (str): message to print
+    """
+    print_lock.acquire()
+    print(message, **kwargs)
+    print_lock.release()
+
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(
+        prog="warplus", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument("-w", "--warpid", help="WARP device ID", required=True)
+
+    parser.add_argument(
+        "-t",
+        "--threads",
+        help="number of threads to use",
+        default=1,
+        type=int,
+    )
+
+    parser.add_argument(
+        "-i",
+        "--interval",
+        help="time interval between sending two requests in one thread",
+        default=15,
+        type=int,
+    )
+
+    parser.add_argument(
+        "-l",
+        "--limit",
+        help="set the maximum number of successful requests the script will send",
+        type=int,
+    )
+
+    parser.add_argument("-o", "--timeout", help="server connection timeout", type=int)
+
+    parser.add_argument("-p", "--proxies", help="use proxies", action="store_true")
+
+    parser.add_argument(
+        "-a",
+        "--autoremove",
+        help="automatically remove unusable proxies",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "-u",
+        "--url",
+        help="URL to send the POST request to",
+        default=f"https://api.cloudflareclient.com/v0a{random.randint(100, 999)}/reg",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="print WARPlus's version and exit",
+        action="store_true",
+    )
+
+    return parser.parse_args()
+
+
+def get_proxies() -> collections.deque:
+    """get a list of proxies from ProxyScrape
+
+    Returns:
+        collections.deque: a deque of retrieved proxies
+    """
+
+    proxies_request = requests.get(
+        "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
+    )
+
+    # if response status code is 200, return the list of retrieved proxies
+    if proxies_request.status_code == requests.codes.ok:
+        proxies = proxies_request.text.split()
+        _print(f"Successfully retried a list of proxies ({len(proxies)} proxies)")
+        return collections.deque(proxies)
+
+    # requests failed to download the list of proxies, raise an exception
+    else:
+        _print("An error occured while retrieving a list of proxies")
+        proxies_request.raise_for_status()
+
+
+# parse command line arguments
+args = parse_arguments()
+
+if args.version:
+    sys.exit(0)
+
+print(f"WARPlus v{VERSION}")
+print("(C) K4YT3X 2021")
+
+# initialize variables for the next section
+print_lock = threading.Lock()
+thread_pool = []
+
+# successes / failures counters
+successes = 0
+fails = 0
+proxies = None
+
+# download a list of proxies if proxies are to be used
+if args.proxies:
+    proxies = get_proxies()
+
+# start threads and add them into the thread pool
+for thread_id in range(args.threads):
+
+    thread = RequestSender(
+        args.warpid, args.interval, args.url, args.limit, args.timeout, args.autoremove
+    )
+
+    thread.name = str(thread_id)
+    _print(f"Starting thread {thread.name}")
+    thread.start()
+    thread_pool.append(thread)
+
+# wait for threads to join
+try:
+    for thread in thread_pool:
+        thread.join()
+
+# if OS kill signal is received or ^C is pressed, stop threads
+except (SystemExit, KeyboardInterrupt):
+    _print("Exit signal received, stopping threads")
+    for thread in thread_pool:
+        thread.stop()
+
+    _print("Waiting for threads to exit")
+    for thread in thread_pool:
+        thread.join()
+
+print("Script finished")
